ce3c1730ab08072e274da07f7814041753dd851f
anthony <anthony.dantard@libcast.com>
2019-01-03 15:39:32 +0100

    Download method done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let client = new Client({
     baseUri: 'https://ws-staging.api.video'
 });
 
-let video = client.videos.search();
+let video = client.videos.download('https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4', 'Test sample');
 video.then(function (result) {
     console.log(result);
 });
