11c3132702770bd71e3701d5ed29bed98646821b
anthony <anthony.dantard@libcast.com>
2019-01-03 17:24:00 +0100

    Begin players API

---
+++ src/Model/player.js
@@ -0,0 +1,37 @@
+class Player {
+    constructor() {
+        this.playerId = null;
+        this.enableApi = null;
+        this.hideTitle = null;
+        this.controlLogo = null;
+        this.buttonRadius = null;
+        this.controlMargin = null;
+        this.forceAutoplay = null;
+        this.controlLogoUrl = null;
+        this.enableControls = null;
+        this.panelTextHover = null;
+        this.scrollbarThumb = null;
+        this.scrollbarTrack = null;
+        this.buttonTextHover = null;
+        this.enableInfoPanel = null;
+        this.panelTextActive = null;
+        this.buttonTextActive = null;
+        this.enableSharePanel = null;
+        this.buttonLightEffect = null;
+        this.panelTextInactive = null;
+        this.trackbarPlayedTop = null;
+        this.trackbarTextColor = null;
+        this.buttonTextInactive = null;
+        this.panelBackgroundTop = null;
+        this.buttonBackgroundTop = null;
+        this.enableDownloadPanel = null;
+        this.enableSettingsPanel = null;
+        this.trackbarPlayedBottom = null;
+        this.panelBackgroundBottom = null;
+        this.trackbarBackgroundTop = null;
+        this.buttonBackgroundBottom = null;
+        this.trackbarBackgroundBottom = null;
+    }
+}
+
+module.exports = Player = null;
