5f6a5dcbe5aed6a42380ed2378fa2b35002b1576
anthony <anthony.dantard@libcast.com>
2019-01-04 16:13:14 +0100

    tokens API

---
+++ src/Api/tokens.js
@@ -0,0 +1,30 @@
+let Tokens = function(browser) {
+
+  this.browser = browser;
+
+  this.generate = async function () {
+    let that = this;
+    let response = await this.browser.post('/tokens');
+
+    return new Promise(function (resolve, reject) {
+      if(!that.browser.isSuccessfull(response)){
+        reject(response);
+      }else{
+        let token = that.cast(response.body);
+        resolve(token);
+      }
+    }).catch(function (e) {
+      console.log(e.statusCode);
+    });
+  };
+};
+
+Tokens.prototype.cast = function(data) {
+  if(!data){
+    return null;
+  }
+
+  return data.token;
+};
+
+module.exports = Tokens;
