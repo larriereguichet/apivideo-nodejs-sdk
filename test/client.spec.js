d8848d7b673203aadfc1b4b0a8dfeaf30148de72
antho <anthony.dantard@libcast.com>
2019-07-19 14:05:52 +0200

    replace old authentication rounting

--- test/client.spec.js
+++ test/client.spec.js
@@ -10,47 +10,43 @@ const expect = require('chai').expect;
 
 describe('apiVideo.Client', () => {
   it('should use the Browser class', () => {
-    const client = new apiVideo.Client({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.Client({ apiKey: 'test' });
     expect(client.browser).instanceOf(apiVideo.Browser);
   });
   it('should use the Videos class', () => {
-    const client = new apiVideo.Client({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.Client({ apiKey: 'test' });
     expect(client.videos).instanceOf(Videos);
   });
   it('should use the Lives class', () => {
-    const client = new apiVideo.Client({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.Client({ apiKey: 'test' });
     expect(client.lives).instanceOf(Lives);
   });
   it('should use the Players class', () => {
-    const client = new apiVideo.Client({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.Client({ apiKey: 'test' });
     expect(client.players).instanceOf(Players);
   });
   it('should use the Captions class', () => {
-    const client = new apiVideo.Client({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.Client({ apiKey: 'test' });
     expect(client.captions).instanceOf(Captions);
   });
   it('should use the Tokens class', () => {
-    const client = new apiVideo.Client({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.Client({ apiKey: 'test' });
     expect(client.tokens).instanceOf(Tokens);
   });
   it('should use the AnalyticsVideo class', () => {
-    const client = new apiVideo.Client({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.Client({ apiKey: 'test' });
     expect(client.analyticsVideo).instanceOf(AnalyticsVideo);
   });
   it('should use the AnalyticsLive class', () => {
-    const client = new apiVideo.Client({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.Client({ apiKey: 'test' });
     expect(client.analyticsLive).instanceOf(AnalyticsLive);
   });
-  it('should set username', () => {
-    const client = new apiVideo.Client({username: 'test', apiKey: 'test'});
-    expect(client.username).eq('test');
-  });
   it('should set apiKey', () => {
-    const client = new apiVideo.Client({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.Client({ apiKey: 'test' });
     expect(client.apiKey).eq('test');
   });
   it('should set baseUri', () => {
-    const client = new apiVideo.Client({username: 'test', apiKey: 'test', baseUri: 'https://test.api.video'});
+    const client = new apiVideo.Client({ apiKey: 'test', baseUri: 'https://test.api.video' });
     expect(client.baseUri).eq('https://test.api.video');
   });
 });
--- test/clientSandbox.spec.js
+++ test/clientSandbox.spec.js
@@ -10,47 +10,43 @@ const expect = require('chai').expect;
 
 describe('apiVideo.ClientSandbox', () => {
   it('should use the Browser class', () => {
-    const client = new apiVideo.ClientSandbox({ username: 'test', apiKey: 'test' });
+    const client = new apiVideo.ClientSandbox({ apiKey: 'test' });
     expect(client.browser).instanceOf(apiVideo.Browser);
   });
   it('should use the Videos class', () => {
-    const client = new apiVideo.ClientSandbox({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.ClientSandbox({ apiKey: 'test' });
     expect(client.videos).instanceOf(Videos);
   });
   it('should use the Lives class', () => {
-    const client = new apiVideo.ClientSandbox({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.ClientSandbox({ apiKey: 'test' });
     expect(client.lives).instanceOf(Lives);
   });
   it('should use the Players class', () => {
-    const client = new apiVideo.ClientSandbox({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.ClientSandbox({ apiKey: 'test' });
     expect(client.players).instanceOf(Players);
   });
   it('should use the Captions class', () => {
-    const client = new apiVideo.ClientSandbox({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.ClientSandbox({ apiKey: 'test' });
     expect(client.captions).instanceOf(Captions);
   });
   it('should use the Tokens class', () => {
-    const client = new apiVideo.ClientSandbox({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.ClientSandbox({ apiKey: 'test' });
     expect(client.tokens).instanceOf(Tokens);
   });
   it('should use the AnalyticsVideo class', () => {
-    const client = new apiVideo.ClientSandbox({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.ClientSandbox({ apiKey: 'test' });
     expect(client.analyticsVideo).instanceOf(AnalyticsVideo);
   });
   it('should use the AnalyticsLive class', () => {
-    const client = new apiVideo.ClientSandbox({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.ClientSandbox({ apiKey: 'test' });
     expect(client.analyticsLive).instanceOf(AnalyticsLive);
   });
-  it('should set username', () => {
-    const client = new apiVideo.ClientSandbox({username: 'test', apiKey: 'test'});
-    expect(client.username).eq('test');
-  });
   it('should set apiKey', () => {
-    const client = new apiVideo.ClientSandbox({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.ClientSandbox({ apiKey: 'test'});
     expect(client.apiKey).eq('test');
   });
   it('should set baseUri to sandbox', () => {
-    const client = new apiVideo.ClientSandbox({username: 'test', apiKey: 'test'});
+    const client = new apiVideo.ClientSandbox({ apiKey: 'test'});
     expect(client.baseUri).eq('https://sandbox.api.video');
   });
 });
