115280bceb250369399f4404a2ff48dfc9ffaf47
lutangar <johan.dufour@gmail.com>
2021-02-09 11:58:46 +0100

    Allow optional `ttl` parameter in `client.tokens.generate` fix #33

--- test/tokens.spec.js
+++ test/tokens.spec.js
@@ -2,15 +2,22 @@ const { expect } = require('chai');
 const apiVideo = require('../lib');
 
 describe('Tokens ressource', () => {
-  describe('generate', () => {
+  describe('generate without specifying a ttl', () => {
     const client = new apiVideo.Client({ apiKey: 'test' });
     it('Does not throw', async () => {
       await client.tokens.generate();
     });
   });
 
+  describe('generate with a custom ttl', () => {
+    const client = new apiVideo.Client({ apiKey: 'test' });
+    it('Does not throw', async () => {
+      await client.tokens.generate(30);
+    });
+  });
+
   describe('cast', () => {
-    it('Should return caption object', () => {
+    it('Should return a token', () => {
       const client = new apiVideo.Client({ apiKey: 'test' });
       const data = { token: 'tox1x1x1x1x1x1x1x1x1x' };
       const token = client.tokens.cast(data);
