62e5b1609a780f63be3c50ef9e94e4e19d7b40bc
lutangar <johan.dufour@gmail.com>
2021-02-19 16:33:15 +0100

    Fix sandbox test
    - Add a lots of expectations
    - Return the right error code in case of failure

--- test/sandbox.spec.js
+++ test/sandbox.spec.js
@@ -1,7 +1,8 @@
 const { expect } = require('chai');
-const got = require('got');
 const apiVideo = require('../lib/index');
 const Caption = require('../lib/Model/caption');
+const Video = require('../lib/Model/video');
+const Player = require('../lib/Model/player');
 
 const timeout = (ms = 100) => new Promise((resolve) => {
   setTimeout(() => {
@@ -22,27 +23,30 @@ const timeout = (ms = 100) => new Promise((resolve) => {
     const client = new apiVideo.ClientSandbox({ apiKey: process.env.API_KEY });
 
     // Create and upload a video ressource
-    const { videoId, title } = await client.videos.upload('test/data/small.webm', { title: 'Course #4 - Part B' });
+    const videoTitle = 'Course #4 - Part B';
+    const { videoId, title } = await client.videos.upload('test/data/small.webm', { title: videoTitle });
+    expect(title).to.equals(videoTitle);
 
     // Update video properties
     const newDescription = 'Course #4 - Part C';
     await client.videos.update(videoId, { description: newDescription }).then((video) => {
-      expect(video.description).to.equals(newDescription);
+      expect(video.videoId).to.equals(videoId);
     });
 
     // Search videos with paginated results
     await client.videos.search({ currentPage: 1, pageSize: 50 }).then((videos) => {
-      videos.forEach(video => console.log(video.videoId));
+      expect(videos).to.be.an('array');
+      videos.forEach(video => expect(video).to.have.keys(new Video()));
     });
 
     // Upload a video thumbnail
     await client.videos.uploadThumbnail('test/data/test.jpg', videoId).then((video) => {
-      expect(video.title).to.equals(title);
+      expect(video.videoId).to.equals(videoId);
     });
 
     // Update video thumbnail by picking image with video timecode
     await client.videos.updateThumbnailWithTimecode(videoId, '00:15:22.05').then((video) => {
-      expect(video.title).to.equals(title);
+      expect(video.videoId).to.equals(videoId);
     });
 
     // Upload video caption
@@ -54,38 +58,35 @@ const timeout = (ms = 100) => new Promise((resolve) => {
 
     // Get video caption by language
     await client.captions.get(videoId, 'en').then((caption) => {
-      console.log(caption.src);
+      expect(caption.src).to.be.a('string');
     });
 
     // Update the default caption language
     await client.captions.updateDefault(videoId, 'en', true).then((caption) => {
-      console.log(caption);
+      expect(caption.srclang).to.equals('en');
+      expect(caption.default).to.equals(true);
     });
 
     // Delete caption by language
     await client.captions.delete(videoId, 'en').then((statusCode) => {
       expect(statusCode).to.equals(204);
-    }).catch((error) => {
-      console.error(error);
     });
 
     // Upload video chapter
     await client.chapters.upload('test/data/en.vtt', { videoId, language: 'en' }).then((chapter) => {
-      console.log(chapter.src);
+      expect(chapter.language).to.equals('en');
     });
 
     await timeout(1000);
 
     // Get video chapter by language
     await client.chapters.get(videoId, 'en').then((chapter) => {
-      console.log(chapter.src);
+      expect(chapter.language).to.equals('en');
     });
 
     // Delete chapter by language
     await client.chapters.delete(videoId, 'en').then((statusCode) => {
-      console.log(statusCode);
-    }).catch((error) => {
-      console.error(error);
+      expect(statusCode).to.equals(204);
     });
 
     // Get video Analytics Data for the current year
@@ -96,19 +97,21 @@ const timeout = (ms = 100) => new Promise((resolve) => {
     // Delete video ressource
     await client.videos.delete(videoId).then((statusCode) => {
       expect(statusCode).to.equals(204);
-    }).catch((error) => {
-      console.error(error);
     });
 
     // Create players with default values
     const { playerId } = await client.players.create();
+    expect(playerId).to.be.a('string');
 
     // Get a player
-    await client.players.get(playerId).then(player => console.log(player.playerId));
+    await client.players.get(playerId).then((player) => {
+      expect(playerId).to.equals(player.playerId);
+    });
 
     // Search a player with paginate results
     await client.players.search({ currentPage: 1, pageSize: 50 }).then((players) => {
-      players.forEach(player => player.playerId);
+      expect(players).to.be.an('array');
+      players.forEach(player => expect(player).to.have.keys(new Player()));
     });
 
     const properties = {
@@ -134,16 +137,18 @@ const timeout = (ms = 100) => new Promise((resolve) => {
       forceLoop: true,
     };
 
-    await client.players.update(playerId, properties).then(player => player.forceLoop);
+    await client.players.update(playerId, properties).then((player) => {
+      Object.keys(properties).forEach((property) => {
+        expect(player).to.have.property(property, properties[property]);
+      });
+    });
 
     await client.players.uploadLogo('test/data/test.jpg', playerId, 'https://api.video').then((player) => {
-      console.log(player.logo);
+      expect(player.logo.link).to.equals('https://api.video');
     });
 
     await client.players.delete(playerId).then((statusCode) => {
-      console.log(statusCode);
-    }).catch((error) => {
-      console.error(error);
+      expect(statusCode).to.equals(204);
     });
 
     // Create a live
@@ -156,16 +161,24 @@ const timeout = (ms = 100) => new Promise((resolve) => {
 
     // Delete live ressource
     await client.lives.delete(liveStreamId).then((statusCode) => {
-      console.log(statusCode);
-    }).catch((error) => {
-      console.error(error);
+      expect(statusCode).to.equals(204);
+    });
+
+    // Create a private live
+    const { liveStreamId: privateLiveStreamId, public: isPublic } = await client.lives.create('This is a private live', { public: false });
+    expect(isPublic).to.equals(false);
+
+    // Delete the private live
+    await client.lives.delete(privateLiveStreamId).then((statusCode) => {
+      expect(statusCode).to.equals(204);
     });
 
     // Generate a token for delegated upload
     await client.tokens.generate().then((token) => {
-      console.log(token);
+      expect(token).to.be.a('string');
     });
   } catch (e) {
     console.error(e);
+    process.exit(1);
   }
 })();
